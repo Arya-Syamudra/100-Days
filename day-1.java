8531b92191122a69d5f1f9e67927f8c3fe597453
Arya-Syamudra <aryasyamudra24@gmail.com>
2023-10-09 22:04:06 +0800

    Latihan Print

---
+++ day-1.java
@@ -0,0 +1,10 @@
+public class Days_1
+{
+   public static void main(String args[])
+   { 
+    System.out.print("Perkenalkan, nama saya ");
+    System.out.println("Arya Syamudra");
+    System.out.print("Mata saya ada ");
+    System.out.println(2);
+   }
+ }
