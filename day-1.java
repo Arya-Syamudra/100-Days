497bb28e04c0ae08b4b8598bbadd5a202889b85b
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-10-09 22:28:27 +0800

    day-1.java

--- day-1.java
+++ day-1.java
@@ -2,8 +2,8 @@ public class Days_1
 {
    public static void main(String args[])
    { 
-    System.out.print("Perkenalkan, nama saya ");
-    System.out.println("Arya Syamudra");
+    System.out.print("Perkenalkan, nama saya "); //Print tanpa baris baru
+    System.out.println("Arya Syamudra"); //Print dengan baris baru setelahnya
     System.out.print("Mata saya ada ");
     System.out.println(2);
    }
