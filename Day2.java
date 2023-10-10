8fb70a756dd6fe3131ab6b4d58bcece695412006
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-10-10 19:15:26 +0800

    Tipe Data Primitif

---
+++ Day2.java
@@ -0,0 +1,21 @@
+ class Day2
+ {
+   public static void main(String args[])
+   { 
+   byte umur = 20; 
+   short kecepatan = 180;
+   int jarak = 50000;
+   long harga = 1000000;
+   float beratBadan = 50.0f;																		double tinggiBadan = 170.0;
+   char ukuranBaju = 'L';
+   boolean hidup = true;
+   System.out.println("Umur = " + umur + " Tahun");
+   System.out.println("Kecepatan = " + kecepatan + " Km/H");
+   System.out.println("Jarak = " + jarak + " Meter");
+   System.out.println("Total Harga = Rp." + harga);
+   System.out.println("Berat Badan = " + beratBadan + " Kg");
+   System.out.println("Tinggi Badan = " + tinggiBadan +" Cm");
+   System.out.println("Ukuran Baju = " + ukuranBaju);
+   System.out.println("Masih hidup ? " + hidup);
+   }
+ }
