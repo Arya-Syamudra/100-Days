29f1ce812dceeab263df2d0d42c88770a9a9c318
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-10-11 22:03:27 +0800

    Ganti Data

---
+++ day-3.java
@@ -0,0 +1,12 @@
+ class Day1
+ {
+   public static void main(String args[])
+   {
+    String nama = "Arya Syamudra";
+    float berat = 50.4f;
+    double tinggi = 160.7;
+    short beratBadan = (short)berat;
+    int tinggiBadan = (int)tinggi;
+    System.out.println("Nama saya " + nama + ". Saya memiliki berat badan " + beratBadan + " kg, dan memiliki tinggi badan " + tinggiBadan + " Cm") ;
+   }
+ }
