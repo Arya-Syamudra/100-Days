32beaca5ae9596487b3eb759fe2d46af13accdc9
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-10-12 21:25:47 +0800

    Ubah tipe data part 2

---
+++ day-4.java
@@ -0,0 +1,11 @@
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    double angka1 = 20.5;
+    int angka2 = (int)angka1;
+    String angka3 = angka2 + "";
+    System.out.println(angka2);
+    System.out.println(angka3);
+   }
+ }
