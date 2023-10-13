d7440bf8560a37c290800c6092707c84a76aefab
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-10-13 22:38:35 +0800

    Ganti tipe data part 3

---
+++ day-5.java
@@ -0,0 +1,12 @@
+ class Day5
+ {
+   public static void main(String args[])
+   { 
+    int harga = 30000;
+    short kembalian = (short)(50000 - harga);
+    byte totalAwal = 0;
+    int totalBelanja = totalAwal + 300000 + 1000000 + 5000;
+    System.out.println("kembalian = " + kembalian);
+    System.out.println("Total belanja anda Rp." + totalBelanja);
+   }
+ }
