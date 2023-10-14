8e2354cec132db5665baf1c8318095ea4a97dc1d
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-10-14 20:47:38 +0800

    Ubah tipe data part kesekian

---
+++ day-6.java
@@ -0,0 +1,11 @@
+class Day6{
+    public static void main(String[] args) {
+        String harga = "12000";
+        int hargaAsli = Integer.parseInt(harga);
+        System.out.println("Harga = " + hargaAsli);
+
+        String berat = "100.2";
+        double beratProduk = Double.parseDouble(berat);
+        System.out.println("Berat Produk = " + beratProduk + " gram");
+    }
+}
