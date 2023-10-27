1db619093bcac3fe42773f9b709aa90e58119ac3
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-10-27 22:22:41 +0800

    Variabel

---
+++ Day-19.java
@@ -0,0 +1,13 @@
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    String namaDepan, namaBelakang;
+    namaDepan = "Arya";
+    namaBelakang = "Zoldyck";
+    System.out.println("Nama saya adalah " + namaDepan + " " + namaBelakang);
+    
+    namaBelakang = "Syamudra";
+    System.out.println("Nama saya adalah " + namaDepan + " " + namaBelakang);
+   }
+ }
