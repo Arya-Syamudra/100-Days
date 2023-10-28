1b6c751f9c8b23cabe1fa9f159f71fa9d25eb948
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-10-28 21:54:12 +0800

    Percanangan If, Else If, Else

---
+++ Day-20.java
@@ -0,0 +1,21 @@
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    int nilai;
+    nilai = 84;
+    if (nilai >= 90){
+      System.out.print("Nilai anda A");
+    } else if (nilai >= 80){
+      System.out.print("Nilai anda B");
+    } else if (nilai >= 70){
+      System.out.print("Nilai anda C");
+    } else if (nilai >= 60){
+      System.out.print("Nilai anda D"); 
+    } else if (nilai >= 50){
+      System.out.print("Nilai anda E");
+    } else {
+      System.out.print("Nilai anda Z");
+    }
+   }
+ }
