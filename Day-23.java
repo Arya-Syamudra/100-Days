de1dfd564d310e4b4ddc8ab068955c7ec16374e9
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-10-31 21:56:36 +0800

    For Looping

--- Day-23.java
+++ Day-23.java
@@ -1,7 +1,7 @@
 class Dcoder {
     public static void main(String[] args) {
         char[] nama = {'A', 'R', 'Y', 'A'};
-        for (int x = 0; x < nama.length; x++) {
+        for (int x = nama.length - 1 ; x >= 0; x--) {
             System.out.print(nama[x]);
         }
     }
