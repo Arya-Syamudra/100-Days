abad3af839550e1d9efd856f1c7bd9b463414d15
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-10-31 21:41:58 +0800

    Perulangan For

---
+++ Day-23.java
@@ -0,0 +1,8 @@
+class Dcoder {
+    public static void main(String[] args) {
+        char[] nama = {'A', 'R', 'Y', 'A'};
+        for (int x = 0; x < nama.length; x++) {
+            System.out.print(nama[x]);
+        }
+    }
+}
