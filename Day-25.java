d47fabf8354700ec20f5e7dadc6241bcec7b681b
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-11-02 23:13:15 +0800

    Do While Looping

---
+++ Day-25.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Dcoder {
+   public static void main(String args[]) { 
+       int angka = 0;
+       do {
+         System.out.println(angka);
+         angka ++;
+       } while (angka < 10);
+   }
+}
