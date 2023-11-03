e30233829e5dc76b2eadc4c5a811c0574e2933ba
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-11-03 23:14:45 +0800

    Array

---
+++ Day-26.java
@@ -0,0 +1,12 @@
+class Dcoder
+ {
+   public static void main(String args[])
+   { 
+     char[] array = {'A', 'R', 'Y', 'A'};
+     int x = 0;
+     while (x < array.length){
+       System.out.print(array[x]);
+       x++;
+     }
+   }
+ }
