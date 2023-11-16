34dab12156454b924aff513cde6fcac1f8f66ed7
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-11-16 23:09:03 +0800

    HashMap

---
+++ Day-39.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+class Dcoder {
+    public static void main(String[] args) {
+        // Membuat objek HashMap dengan key bertipe String dan value bertipe Integer
+        HashMap<String, Integer> umur = new HashMap<>();
+
+        umur.put("Jojo", 35);
+        umur.put("Joro", 20);
+        umur.put("Jono", 25);
+
+        int umurJoro = umur.get("Joro");
+        System.out.println("Umur umur Joro adalah " + umurJoro);
+
+        umur.remove("Jono");
+
+        
+        for (String nama : umur.keySet()) {
+            System.out.println(nama + " : " + umur.get(nama));
+        }
+
+        System.out.println(umur.containsKey("Jojo"));
+    }
+}
