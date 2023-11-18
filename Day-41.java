6d18b0f15f1e8c3aa780339583dd54dd916beca9
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-11-18 23:29:56 +0800

    Continue

---
+++ Day-41.java
@@ -0,0 +1,12 @@
+class Dcoder
+ {
+   public static void main(String args[])
+   { 
+     for (int x = 1; x <= 10; x++){
+       if (x % 2 == 0){
+         continue;
+       }
+     System.out.println(x);
+     }
+   }
+ }
