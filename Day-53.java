4e92757858dfbc57fdfc876dec6657965c3fcd88
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2023-11-30 23:06:48 +0800

    Add files via upload

---
+++ Day-53.java
@@ -0,0 +1,13 @@
+class Dcoder
+{
+  public static void main(String args[])
+  {
+    int i, j;
+    for (i=1; i<=5; i++) {
+      for (j=1; j<=i; j++) {
+        System.out.print("*"); 
+      }
+    System.out.println(); //ini untuk menciptakan line baru
+    }
+  }
+}
