1a1642c02555d131d2b5624753b43f65c89543b1
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2024-01-07 22:40:28 +0800

    .

--- Day-91.java
+++ Day-91.java
@@ -2,7 +2,7 @@ class Dcoder
 {
   public static void main(String args[])
   { 
-    sapa("Arya Syamudra", 20); CV s
+    sapa("Arya Syamudra", 20);
   }
   static void sapa (String nama, int usia){
     System.out.println("Hai, " + nama + ". Saya Ramal Anda Berusia " + usia + " Tahun.");
