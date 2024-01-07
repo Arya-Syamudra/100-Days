57a5e63dad59bb5e3dc9df1f78760a21b632ab2f
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2024-01-07 06:36:31 -0800

    Add files via upload

---
+++ Day-91.java
@@ -0,0 +1,10 @@
+class Dcoder
+{
+  public static void main(String args[])
+  { 
+    sapa("Arya Syamudra", 20); CV s
+  }
+  static void sapa (String nama, int usia){
+    System.out.println("Hai, " + nama + ". Saya Ramal Anda Berusia " + usia + " Tahun.");
+  }
+}
